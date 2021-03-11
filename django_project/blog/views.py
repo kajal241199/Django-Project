3a64ad437e54ffbaeebbe637f6534b8ba5ef6200
Kajal Singhvi <kajal.singhvi@gatewaygroup.com>
2021-03-11 15:41:55 +0530

    Added user profile page and functionality to update their profile picture and information

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,19 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<view_type>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
+
+class PostDetailView(DetailView):
+    model = Post
+    template = 'blog/post_detail.html'
+    
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
